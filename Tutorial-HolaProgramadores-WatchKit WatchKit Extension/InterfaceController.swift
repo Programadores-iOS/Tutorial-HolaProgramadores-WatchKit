aec4e37a0ea2dc9a9328f3d3bdd3697674dbde04
Programadores-iOS.net <github@programadores-ios.net>
2014-11-18 19:13:18 -0800

    Tutorial-HolaProgramadores-WatchKit
    Un vistazo al nuevo WatchKit para crear Apple Watch Apps

---
+++ Tutorial-HolaProgramadores-WatchKit WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,35 @@
+//
+//  InterfaceController.swift
+//  Tutorial-HolaProgramadores-WatchKit WatchKit Extension
+//
+//  Created by Fernando Medina on 11/18/14.
+//  Copyright (c) 2014 Programadores-iOS.net. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override init(context: AnyObject?) {
+        // Initialize variables here.
+        super.init(context: context)
+        
+        // Configure interface objects here.
+        NSLog("%@ init", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+}
