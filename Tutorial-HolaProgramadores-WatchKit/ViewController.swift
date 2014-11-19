028ed77b3f3dbef728ed9a9508dd4800531ae336
Programadores-iOS.net <github@programadores-ios.net>
2014-11-18 18:48:59 -0800

    Initial Commit

---
+++ Tutorial-HolaProgramadores-WatchKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tutorial-HolaProgramadores-WatchKit
+//
+//  Created by Fernando Medina on 11/18/14.
+//  Copyright (c) 2014 Programadores-iOS.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
